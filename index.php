27c24f65910b064eaa4820be86a92a38fe3758a7
Izaias Cavalcanti <izaias@iuqo.com>
2016-04-29 20:34:12 -0300

    teste 9

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <body>
 	<div class="main">
 		<!-- <video id="my-video" class="video-js" loop="true" preload="auto" autoplay="" src="video/32x32-audio.mp4"> -->
+		<video id="my-video" class="video-js" loop controls preload="auto" autoplay width="640" height="264">
 		<video id="my-video" class="video-js" controls preload="auto" width="640" height="264">
 	        <source src="video/32x32-audio.mp4" type="video/mp4">
 	        <source src="video/32x32-audio.webm" type="video/webm">
@@ -23,21 +24,3 @@
 	<script src="https://vjs.zencdn.net/5.9.2/video.js"></script>
 </body>
 </html>
-
-<!-- 
-
-
-
-<body>
-  <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
-  poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
-    <source src="MY_VIDEO.mp4" type='video/mp4'>
-    <source src="MY_VIDEO.webm" type='video/webm'>
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a web browser that
-      <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-    </p>
-  </video>
-
-  <script src="http://vjs.zencdn.net/5.9.2/video.js"></script>
-</body> -->
